18d004083db9bdad3dc52762501f8b17b1629f3b
KostasEreksonas <k.ereksonas@gmail.com>
2023-12-05 20:57:14 +0200

    Update

--- 2023/Day2/solution1.py
+++ 2023/Day2/solution1.py
@@ -7,6 +7,12 @@ count = {"red":0, "green":0, "blue":0}
 with open ("input", 'r') as f:
     games = f.read().split("\n")
 
+del games[-1]
+
 for game in games:
-    values = game.split(":")[1].split(";")
-    print(values)
+    #g = game.split(": ")[-1].split("; ")[-1].split(", ")
+    g = game.split(": ")
+    ids = g[0].split()[-1]
+    for x in range(len(g[1].split("; "))):
+        print(g[1].split(", ")[x])
+    #print(f"{game}, {type(game)}")
